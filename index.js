9397b8fd7c916dbc217bc64bbe2569ac82e1c88a
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2024-07-28 19:48:00 +0300

    Update 28/07/2024 19:48

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// SideBar
 function showSidebar () {
     const sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "flex"
@@ -8,6 +9,7 @@ function hideSidebar () {
     sidebar.style.display = "none"
 }
 
+// DarkMode
 let darkmode = localStorage.getItem("darkmode")
 const themeSwitch = document.getElementById("theme-switch")
 
@@ -21,9 +23,17 @@ const disableDarkmode = () => {
     localStorage.setItem("darkmode", null)
 }
 
+const img1 = () => {
+    document.getElementById("img1").src="../Imgs/desc-1-light.png";
+    
+}
+
 if (darkmode ==="active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem("darkmode")
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode !== "active" ? document.getElementById("img1").src="../Imgs/desc-1-light.png" : document.getElementById("img1").src="../Imgs/desc-1-dark.png";
+    darkmode !== "active" ? document.getElementById("img2").src="../Imgs/desc-2-light.png" : document.getElementById("img2").src="../Imgs/desc-2-dark.png";
+    darkmode !== "active" ? document.getElementById("img3").src="../Imgs/desc-3-light.png" : document.getElementById("img3").src="../Imgs/desc-3-dark.png";
 })
