01844df881b7d8479749b62f4b8f3d4395243a52
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2024-07-28 20:23:10 +0300

    Update 28/07/2024 20:23

--- index.js
+++ index.js
@@ -16,24 +16,23 @@ const themeSwitch = document.getElementById("theme-switch")
 const enableDarkmode = () => {
     document.body.classList.add("darkmode")
     localStorage.setItem("darkmode", "active")
+    document.getElementById("img1").src="../Imgs/desc-1-light.png"
+    document.getElementById("img2").src="../Imgs/desc-2-light.png"
+    document.getElementById("img3").src="../Imgs/desc-3-light.png"
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove("darkmode")
     localStorage.setItem("darkmode", null)
-}
-
-const img1 = () => {
-    document.getElementById("img1").src="../Imgs/desc-1-light.png";
-    
+    document.getElementById("img1").src="../Imgs/desc-1-dark.png";
+    document.getElementById("img2").src="../Imgs/desc-2-dark.png";
+    document.getElementById("img3").src="../Imgs/desc-3-dark.png";
 }
 
 if (darkmode ==="active") enableDarkmode()
 
 themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem("darkmode")
+
     darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-    darkmode !== "active" ? document.getElementById("img1").src="../Imgs/desc-1-light.png" : document.getElementById("img1").src="../Imgs/desc-1-dark.png";
-    darkmode !== "active" ? document.getElementById("img2").src="../Imgs/desc-2-light.png" : document.getElementById("img2").src="../Imgs/desc-2-dark.png";
-    darkmode !== "active" ? document.getElementById("img3").src="../Imgs/desc-3-light.png" : document.getElementById("img3").src="../Imgs/desc-3-dark.png";
 })
