3afe9058fb269e0baa6153db159e1234c8949792
Antonis Rusman <75878415+AntonisRsmn@users.noreply.github.com>
2024-07-28 17:33:02 +0300

    Update 28/07/2024 17:33

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function hideSidebar () {
     const sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "none"
 }
+
+let darkmode = localStorage.getItem("darkmode")
+const themeSwitch = document.getElementById("theme-switch")
+
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode")
+    localStorage.setItem("darkmode", "active")
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode")
+    localStorage.setItem("darkmode", null)
+}
+
+if (darkmode ==="active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode")
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
